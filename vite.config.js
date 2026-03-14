13ae9d177064d3928a97b398670e0fb62d3201cc
YukoSmile <yukodivert@gmail.com>
2026-03-14 10:42:01 +0100

    Updated hierarchy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/PortfolioYuko-React/',
   plugins: [react()],
-  base: "/Portfolio/",
+  base: "/PortfolioYuko-React/",
 });
