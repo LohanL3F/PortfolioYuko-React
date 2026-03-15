b910a5974fe9468648e7a227b6e68c1f25d0ba2f
YukoSmile <yukodivert@gmail.com>
2026-03-15 01:31:18 +0100

    Profile page now available

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import music from "../assets/Musiques/BreakingGround.mp3";
+import soundIcon from "../assets/Images/sound.png";
+import noSoundIcon from "../assets/Images/Nosound.png";
 import "./Page.css";
+import ProfileBackground from "../components/ProfileBackground";
 import { useNavigate } from "react-router-dom";
+import lightClick from "../assets/Musiques/lightClick.mp3";
 
-export default function Profil() {
+export default function Profile() {
   const navigate = useNavigate();
+  const audioRef = useRef(null);
+  const [muted, setMuted] = useState(false);
+
+  const toggleSound = () => {
+    const audio = audioRef.current;
+    audio.muted = !muted;
+    setMuted(!muted);
+  };
+
+  const playLightClick = () => {
+    const audio = new Audio(lightClick);
+    audio.volume = 0.4;
+    audio.play().catch(() => {});
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.volume = 0.4;
+    audio.loop = true;
+
+    // Play on first user interaction (click or touch)
+    const startMusic = () => {
+      audio.play().catch(() => {});
+      window.removeEventListener("click", startMusic);
+    };
+
+    window.addEventListener("click", startMusic);
+
+    // Cleanup (remove event listener on unmount)
+    return () => window.removeEventListener("click", startMusic);
+  }, []);
 
   return (
-    <div className="page fade">
-      <h1 id="devMsg">🚧 Page en cours de développement... 🚧</h1>
-      <h2 id="devMsg2">Womp womp.. :(</h2>
+    <div className="page">
+      <ProfileBackground />
+
       <button className="back-button" onClick={() => navigate("/")}>
         ←
+      </button>
+      <audio ref={audioRef} src={music} />
+      <button
+        className="sound-button"
+        onClick={() => {
+          toggleSound();
+          playLightClick();
+        }}
+      >
+        <img src={muted ? noSoundIcon : soundIcon} alt="sound toggle" />
       </button>
     </div>
   );
